3ddae0de30429bc22d2d2e3dae5652f35bdde9fa
Evgeniy Buzo <buzoevgenij661@gmail.com>
2025-03-07 11:46:19 +0300

    resolve all linear tasks

---
+++ Lessons/TypeOperationsWorkshop/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace TypeOperationsWorkshop;
+
+class Program
+{
+	/*
+    static void Main(string[] args)
+    { 
+		double C = 22.5;
+		double F = C * 9.0 / 5.0 + 32;
+		
+	
+	
+        Console.WriteLine(F);
+    }
+*/
+
+		//1
+	static void Main(string[] args)
+	{
+		double P = 3.14159;
+		double r = 9;
+		double S = P * r * r;
+		
+		Console.WriteLine(S);
+	}
+		
+		//2
+	/*
+	static void Main(string[] args)
+	{
+		double a = 2;
+		double b = 6;
+		double P = 2 * (a + b);
+		double S = a * b ;
+		
+		
+		Console.WriteLine(S);
+	}
+	
+		//3 
+	static void Main(string[] args)
+	{
+		double X = 10;
+		double km = X;
+		double m = X * 1000;
+		
+		Console.WriteLine(X);
+	}
+	
+		//4
+	static void Main(string[] args)
+	{
+		double a = 5;
+		double P = 4 * a;
+		
+		Console.WriteLine(P);
+	}
+		
+		//5
+	static void Main(string[] args)
+	{
+		double number1 = 3;
+		double number2 = 7;
+		double number3 = 13;
+		double average = (number1 + number2 + number3) / 3;
+		Console.Writeline(average);
+	}
+	
+		//6
+	static void Main(string[] args)
+	{
+		double min = 94;
+		double h = 94 / 60;
+		min = min % 60;
+		
+		Console.WriteLine(min);
+		Console.WriteLine(h);
+	}
+	*/
+}
