938b399961254fec6b43a9987d3b6f9e097e1990
Evgeniy Buzo <buzoevgenij661@gmail.com>
2025-03-31 17:08:32 +0300

    refactering temperares

--- Lessons/TypeOperationsWorkshop/Program.cs
+++ Lessons/TypeOperationsWorkshop/Program.cs
@@ -2,79 +2,93 @@
 
 class Program
 {
-	/*
-    static void Main(string[] args)
-    { 
-		double C = 22.5;
-		double F = C * 9.0 / 5.0 + 32;
-		
-	
-	
-        Console.WriteLine(F);
-    }
-*/
-
-		//1
 	static void Main(string[] args)
 	{
-		double P = 3.14159;
-		double r = 9;
-		double S = P * r * r;
+		Console.WriteLine("Перевод байтов в килобайты");
+		double bites = 3500;
+		Console.WriteLine("Байты: " + bites);
+		double kilobites = bites / 1024;
+		Console.WriteLine("Килобайты: " + kilobites);
+		Console.WriteLine();
 		
-		Console.WriteLine(S);
-	}
+		Console.WriteLine("Перевод часы в минуты");
+		double minutes = 94;
+		Console.WriteLine("Минуты: " + minutes);
+		double hours = 94 / 60;
+		Console.WriteLine("Часы: " + hours);
+		minutes = minutes % 60;	
+		Console.WriteLine("Минут: " + minutes);
+		Console.WriteLine();
 		
-		//2
-	/*
-	static void Main(string[] args)
-	{
+		Console.WriteLine("Вычисление среднеарифмитического");
+		double firstNumber = 3;
+		Console.WriteLine("Первое число: " + firstNumber);
+		double secondNumber = 7;
+		Console.WriteLine("Второе число: " + secondNumber);
+		double thirdNumber = 13;
+		Console.WriteLine("Третие число: " + thirdNumber);
+		double average = (firstNumber + secondNumber + thirdNumber) / 3;
+		Console.WriteLine("Среднеарифмитическое: " + average);
+		Console.WriteLine();
+		
+		Console.WriteLine("Вычисление периметра квадрата");
+		double squareSide = 5;
+		Console.WriteLine("Сторона квадрата: " + squareSide);
+		double squarePerimetr = 4 * squareSide;
+		Console.WriteLine("Периметр квадрата: " + squarePerimetr);
+		Console.WriteLine();
+		
+		Console.WriteLine("Перевод км в м");
+		double kilometers = 10;
+		Console.WriteLine("Километры: " + kilometers);
+		double meters = kilometers * 1000;
+		Console.WriteLine("Метры: " + meters);
+		Console.WriteLine();
+		
+		Console.WriteLine("Поиск площади прямоугольника");
 		double a = 2;
+		Console.WriteLine("a: " + a);
 		double b = 6;
-		double P = 2 * (a + b);
-		double S = a * b ;
+		Console.WriteLine("b: " + b);
+		double perimetr = 2 * (a + b);
+		Console.WriteLine("Периметр прямоугольника: " + perimetr);
+		double rectangleSquare = a * b ;
+		Console.WriteLine("Площадь прямоугольника: " + rectangleSquare);
+		Console.WriteLine();
 		
+		Console.WriteLine("Поиск площади");
+		double radius = 9;
+		Console.WriteLine("Радиус: " + radius);
+		double square = Math.PI * radius * radius;
+		Console.WriteLine("Площадь: " + square);
+		Console.WriteLine();
 		
-		Console.WriteLine(S);
+		Console.WriteLine("Перевод градусов цельсия в Фаренгейта");
+		double celsius = 22.5;
+		Console.WriteLine("Градусы по цельсию: " + celsius);
+		double fahrenheit = celsius * 9.0 / 5.0 + 32;		
+        Console.WriteLine("Температура по фаренгейту: " + fahrenheit);
+		Console.WriteLine();
+		
+		Console.WriteLine("Получение следующего символа в ASCII таблице по заданному");
+		char symbol = 'Q';
+		Console.WriteLine("Заданый символ: " + symbol);
+		char nextSymbol = (char)(symbol + 1);
+		Console.WriteLine("Следующий символ: " + nextSymbol);
+		
+		Console.WriteLine("Вывод символа по номеру из таблицы ASCII");
+		int charNumber = 97;
+		Console.WriteLine("Номер символа: " + charNumber);
+		char charByNumber = (char) charNumber;
+		Console.WriteLine("Символ: " + charByNumber);
+		
+		Console.WriteLine("Расчет стоимости товара");
+		double price = 1000;
+		Console.WriteLine("Цена: " + price);
+		int discount = 15;
+		Console.WriteLine("Скидка: " + discount);
+		double cost = price - price * discount / 100;
+		Console.WriteLine("Итоговая стоимость: " + cost);
 	}
 	
-		//3 
-	static void Main(string[] args)
-	{
-		double X = 10;
-		double km = X;
-		double m = X * 1000;
-		
-		Console.WriteLine(X);
-	}
-	
-		//4
-	static void Main(string[] args)
-	{
-		double a = 5;
-		double P = 4 * a;
-		
-		Console.WriteLine(P);
-	}
-		
-		//5
-	static void Main(string[] args)
-	{
-		double number1 = 3;
-		double number2 = 7;
-		double number3 = 13;
-		double average = (number1 + number2 + number3) / 3;
-		Console.Writeline(average);
-	}
-	
-		//6
-	static void Main(string[] args)
-	{
-		double min = 94;
-		double h = 94 / 60;
-		min = min % 60;
-		
-		Console.WriteLine(min);
-		Console.WriteLine(h);
-	}
-	*/
 }
